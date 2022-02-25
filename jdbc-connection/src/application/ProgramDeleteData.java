34ce5f468aa3d9aa7da7dd594a590437cdeb655d
AlanGustav0 <alangustavo946@gmail.com>
2022-02-25 15:14:43 -0300

    Delete data on database

--- jdbc-connection/src/application/ProgramDeleteData.java
+++ jdbc-connection/src/application/ProgramDeleteData.java
@@ -1,4 +1,33 @@
 package application;
 
+import db.Database;
+import db.DbIntegrityException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ProgramDeleteData {
+
+    public static void main(String[] args) {
+
+        //<--DELETANDO DADOS
+        Connection connection = null;
+        PreparedStatement st = null;
+
+        try {
+            connection = Database.getConnection();
+            st = connection.prepareStatement(
+                    "delete from department " +
+                            "where id = ?"
+            );
+            st.setInt(1,4);
+
+            int rowsAffected = st.executeUpdate();
+            System.out.println(String.format("Done! Rows affected = %d", rowsAffected));
+
+        } catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage());
+        }
+    }
 }
